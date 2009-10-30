64f9c7ba06f264b17cd38969312bcb89aa7459e3
zark zork <zarkzork@atrix.jnetx.ru>
2009-10-30 18:20:00 +0300

    small refactor on diagram draw and added some comments.

--- canvas.js
+++ canvas.js
@@ -1,5 +1,5 @@
 $(onLoad);
-
+/* preload images that will be used in builder */
 var preloaded_images=new PreloadedImages(["anno",
 					  "pin",
 					  "black",
@@ -12,6 +12,8 @@ var preloaded_images=new PreloadedImages(["anno",
 					  "start",
 					  "usa"]);
 
+/* @return object containing preloaded images objects. When all images */
+/* will be loaded. this.images loaded will be true.*/
 function PreloadedImages(images){
   var self=this;
   var count=images.length;
@@ -38,6 +40,7 @@ function PreloadedImages(images){
 }
 
 function onLoad(){
+  /* if images has not been loaded wait little */
   if(!preloaded_images.images_loaded){
     setTimeout(onLoad, 20);
   }else{
@@ -46,10 +49,14 @@ function onLoad(){
   }
 }
 
+/* global constants describing size of feature box */
 var FEATURE_HEIGTH=110;
 var SHORT_WIDTH=100;
 var LONG_WIDTH=200;
 
+/* objects on scheme are representet with nodes. Each node contain  */
+/* clone of a feature and it's children which should be nodes or null  */
+/* objects */
 function Node(feature){
   this.feature=!feature?null:feature.clone();
   this.children=[];
@@ -62,10 +69,13 @@ function Node(feature){
 }
 
 Node.prototype={
+  /* remove feature and its children from node */
   clear: function(){
     this.feature=null;
     this.children=[];
   },
+  /* recursively determines height of node. @return integer
+   representing number of objects that needs to be stored in node */
   getHeight: function(){
     if(!this.feature||this.feature.transitions==0){
       return 1;
@@ -76,6 +86,9 @@ Node.prototype={
     }
     return height;
   },
+  /* returns canvas with transitions. Also adds small icon
+  representing each transition. If icon is absent draws a circle with
+  random color */
   getCanvas: function(){
     var cnvs=Canvas("", this.getHeight()*FEATURE_HEIGTH, LONG_WIDTH);
     var ctx=cnvs[0].getContext("2d");
@@ -86,19 +99,23 @@ Node.prototype={
       ctx.beginPath();
       var start_x=SHORT_WIDTH/2;
       var start_y=FEATURE_HEIGTH*this.getHeight()/2;
+      /* move to start of line */
       ctx.moveTo(start_x, start_y);
       var end_x=LONG_WIDTH-5;
       var end_y=current_height+
 	FEATURE_HEIGTH*this.children[i].getHeight()/2;
       current_height+=this.children[i].getHeight()*FEATURE_HEIGTH;
+      /* draw line */
       ctx.lineTo(end_x, end_y);
       ctx.closePath();
       ctx.stroke();
       ctx.beginPath();
+      /* draw white circle at the end of line */
       ctx.arc(end_x, end_y, 5, 0, Math.PI*2, true);
       ctx.fill();
       ctx.beginPath();
       if(this.feature.options.transitions_img){
+	/* draw image in the middle of line */
 	ctx.drawImage(this.feature.options.transitions_img[i],
 		      start_x+(end_x-start_x)/2-10,
 		      start_y+(end_y-start_y)/2-10,
@@ -106,6 +123,7 @@ Node.prototype={
 	ctx.fill();
       }else{
 	ctx.fillStyle= "rgb"+this.feature.options.node_colors[i];
+	/* draw circle */
 	ctx.arc(start_x+(end_x-start_x)/2,
 		start_y+(end_y-start_y)/2,
 		5, 0, Math.PI*2, true);
@@ -116,7 +134,8 @@ Node.prototype={
   }
 };
 
-
+/* place where scheme is stored. Object controls position of nodes and*/
+/* draws them to #workspace */
 function Workspace(){
   this.root=new Node(new Feature(1,{},{img:preloaded_images.start}));
   this.draw();
@@ -124,6 +143,7 @@ function Workspace(){
 }
 
 Workspace.prototype={
+  /* remove node from workspace */
   remove: function(node){
     if(node==this.root){
       throw "can't delete root node";
@@ -131,70 +151,98 @@ Workspace.prototype={
     node.clear();
     this.draw();
   },
+  /* append node @node -- node to be appended. @pos -- position among
+  node transitions and @feature -- feature to be added. */
   append: function(node,pos,feature){
     node.children[pos]=new Node(feature);
     this.draw();
   },
-  draw: function(node,deep,height){
+  /* Places transitions in the right place on #workspace */
+  drawTransition: function(node, deep, height){
+    var transitions=node.getCanvas();
+    var left_pos=deep*LONG_WIDTH-deep*SHORT_WIDTH/2;
+    transitions
+      .css("position","absolute")
+      .css("top", height*FEATURE_HEIGTH)
+      .css("left", left_pos);
+    $("#diagram").append(transitions);
+  },
+  /* Places Feature in the rigth place of the #workspace */
+  drawFeature: function(node, deep, height){
+    var left_pos=deep*LONG_WIDTH-deep*SHORT_WIDTH/2;
+    var feature;
+    if(node==this.root){
+      feature=node.feature.getCanvas();	
+    }else{
+      feature=node.feature.getCanvas();
+      feature.click(function(){
+		      var menu=new Menu(node, self);
+		      menu.showMenu();
+		    });
+    }
+    var feature_top=(node.getHeight()/2+height)*FEATURE_HEIGTH
+       	-FEATURE_HEIGTH/2;
+    feature
+      .css("position","absolute")
+      .css("top", feature_top)
+      .css("left", left_pos);
+    $("#diagram").append(feature);      
+  },
+  /* Draws a box which is used as container that gets droppable
+  objects (see jquery droppable and draggable). @i -- position in the
+  node to which dropped feature will be appended. for other params see
+  this.draw() */
+  drawBox: function(i, node, deep, height){
     var self=this;
-    if(!node){
+    var left_pos=deep*LONG_WIDTH-deep*SHORT_WIDTH/2;
+    var droppable=$("<div/>")
+      .css("position","absolute")
+      .addClass("border")
+      .css("height", FEATURE_HEIGTH)
+      .css("top", height*FEATURE_HEIGTH)
+      .css("left", left_pos+SHORT_WIDTH+SHORT_WIDTH/2)
+      .css("width", SHORT_WIDTH)
+      .html("&nbsp;");
+    /* preventing closure in drop function */
+    (function(i,node,droppable){
+       droppable.droppable({
+			     drop:function(e, ui){
+			       self.append(node,
+					   i,
+					   ui.draggable.data("self"));
+			     }
+			   });	     
+     })(i,node,droppable);
+    $("#diagram").append(droppable);    
+  },
+  /* Draws whole tree on the workspace. Should be used without
+  args. function recursively calls itself to draw each node */
+  draw: function(){
+    /* if there is no arguments call self with root node */
+    if(arguments.length==0){
       $("#diagram").empty();
       $("#diagram").css("height", this.root.getHeight()*FEATURE_HEIGTH);
       return this.draw(this.root,0,0);
     }else{
+      /* node to be drawn */
+      var node=arguments[0];
+      /* level of deepness (left shift) */
+      var deep=arguments[1];
+      /* current distance from top in feature heights. */
+      var height=arguments[2];
       /* draw transitions */
-      var transitions=node.getCanvas();
-      var left_pos=deep*LONG_WIDTH-deep*SHORT_WIDTH/2;
-      transitions
-	.css("position","absolute")
-	.css("top", height*FEATURE_HEIGTH)
-	.css("left", left_pos);
-      $("#diagram").append(transitions);
-      /* draw box */
-      var feature;
-      if(node==this.root){
-	feature=node.feature.getCanvas();	
-      }else{
-	feature=node.feature.getCanvas();
-	feature.click(function(){
-			var menu=new Menu(node, self);
-			menu.showMenu();
-		      });
-      }
-      var feature_top=transitions.height()/2
-       	+height*FEATURE_HEIGTH
-       	-FEATURE_HEIGTH/2;
-      feature
-       	.css("position","absolute")
-       	.css("top", feature_top)
-       	.css("left", left_pos);
-      $("#diagram").append(feature);
+      this.drawTransition(node,deep,height);
+      /* draw feature icon */
+      this.drawFeature(node, deep, height);
       var used_height=0;
       var child;
+      /* calls it self recursively adding one to deep and height */
       for(var i in node.children){
 	child=node.children[i];
 	if(child.feature){
 	  used_height+=this.draw(child,deep+1,height+used_height);
 	}else{
-	  var droppable=$("<div/>")
-	    .css("position","absolute")
-	    .addClass("border")
-	    .css("height",FEATURE_HEIGTH)
-	    .css("top",(height+used_height)*FEATURE_HEIGTH)
-	    .css("left",left_pos+SHORT_WIDTH+SHORT_WIDTH/2)
-	    .css("width", SHORT_WIDTH)
-	    .html("&nbsp;");
-	  /* preventing closure in drop function */
-	  (function(i,node,droppable){
-	     droppable.droppable({
-				   drop:function(e, ui){
-				     self.append(node,
-						 i,
-						 ui.draggable.data("self"));
-				   }
-				 });	     
-	   })(i,node,droppable);
-	  $("#diagram").append(droppable);
+	  this.drawBox(i, node, deep, height+used_height);
 	  used_height++;
 	}
       }
@@ -207,6 +255,8 @@ Workspace.prototype={
   }
 };
 
+/* pop up to set up feature parameters wich are stored in */
+/* node.properties */
 function Menu(node, workspace){
   this.node=node;
   this.properties=node.feature.properties;
@@ -261,6 +311,12 @@ Menu.prototype={
   
 };
 
+/* Represents feature. @transitions -- amount of possible outputs of  */
+/* this feature, @properties -- object containing feature properties  */
+/* and @option is objects for storing feature representation. It can  */
+/* contain field 'img' which should be loaded Image objects, field  */
+/* 'transitions_img' which should be array of loaded objects, name --  */
+/* feature name.  */
 function Feature(transitions,properties,options){
   this.transitions=transitions||0;
   this.properties=properties||{};
@@ -280,6 +336,8 @@ function Feature(transitions,properties,options){
 }
 
 Feature.prototype={
+  /* copies properties field and return new Feature with new
+   properties object */
   clone: function(){
     var properties={};
     for(var i in this.properties){
@@ -287,6 +345,7 @@ Feature.prototype={
     }
     return new Feature(this.transitions, properties, this.options);
   },
+  /* return canvas containing feature icon */
   getCanvas: function(){
     var self=this;
     var height;
@@ -308,7 +367,7 @@ Feature.prototype={
     return cnvs;
   },
   /* feature box with caption on palette */
-  featureBox: function (text){
+  featureBox: function (){
     var self=this;
     var canvas=this.getCanvas();
     canvas.draggable({
@@ -324,12 +383,13 @@ Feature.prototype={
       .append(canvas);
     var caption=$("<div/>")
       .addClass("caption")
-      .text(text);
+      .text(this.options.name);
     box.append(caption);
     return $("<div>").append(box);
   }
 };
 
+/* hides all initial screen helpers */
 function hideHelpers(){
     $(".helper").fadeOut("slow");
 }
@@ -344,6 +404,7 @@ function Canvas(id, height, width){
     .css("width",width);
 }
 
+/* palette with features that will be used to build diagram */
 function Palette(){
   this.append(new Feature(3,
 			  {
@@ -396,13 +457,14 @@ function Palette(){
 			  }),
 	      "Connect");
   
-  this.append(new Feature(1,
+  this.append(new Feature(2,
 			  {
 			    "block regexp":"+7495*"
 			  },
 			  {
 			    img:preloaded_images.black,
-			    transitions_img:[preloaded_images.ok]
+			    transitions_img:[preloaded_images.ok,
+					     preloaded_images.error]
 			  }),
 	      "Black list");
   return this;
